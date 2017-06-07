0bf0a473cf3ff3233a7cbac702d12c5b993ab849
Monte Jiran <montejiran@gmail.com>
2017-06-07 10:12:06 -0700

    add gen token on signup, and pass down

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,7 +9,7 @@ class Api::UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save
-			login(@user)
+      @user.generate_auth_token
 			render :show
 		else
 			render json: @user.errors.full_messages, status: 422
