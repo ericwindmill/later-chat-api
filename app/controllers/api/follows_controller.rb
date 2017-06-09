0d34c8dc50e280a869f73527927e24524ca87f37
Monte Jiran <montejiran@gmail.com>
2017-06-09 11:22:12 -0700

    follow destroy find

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,6 +1,7 @@
 class Api::FollowsController < ApplicationController
 
   def create
+    debugger
     @follow = Follow.new(follower_id: params[:follower_id], leader_id: params[:leader_id])
     if @follow.save
       # returns the id of the leader to be added to the current
@@ -12,7 +13,7 @@ class Api::FollowsController < ApplicationController
   end
 
   def destroy
-    @follow = Follow.find_by(follow_params)
+    @follow = Follow.find_by(follower_id: params[:follower_id], leader_id: params[:leader_id])
     @follow.destroy
     # returns the id of the leader to be removed from the current
     # user's leaders array
