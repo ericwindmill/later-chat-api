83e681e82355a43b30f9ba6f37b4e1965d4d0fb1
Grey Keith <r.grey.keith@gmail.com>
2017-06-08 17:32:18 -0700

    add notation

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -3,6 +3,8 @@ class Api::FollowsController < ApplicationController
   def create
     @follow = Follow.new(follow_params)
     if @follow.save
+      # returns the id of the leader to be added to the current
+      # user's leaders array
       render json: @follow.leader_id
     else
       render json: @follow.errors.full_messages, status: 422
@@ -12,6 +14,8 @@ class Api::FollowsController < ApplicationController
   def destroy
     @follow = Follow.find_by(follow_params)
     @follow.destroy
+    # returns the id of the leader to be removed from the current
+    # user's leaders array
     render json: @follow.leader_id
   end
 
