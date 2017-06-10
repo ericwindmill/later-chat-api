20340c270c4cd0e293d8033435b3e3100e52afe0
Monte Jiran <montejiran@gmail.com>
2017-06-10 14:45:20 -0700

    remove commented out code

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,3 @@ Rails.application.routes.draw do
     get    'verify'  => 'sessions#verify_access_token'
   end
 end
-
-
-#  namespace :api, :defaults => {:format => :json} do
-#     as :user do
-#       post   "/sign-in"       => "sessions#create"
-#       delete "/sign-out"      => "sessions#destroy"
-#     end
-#   end
